7e318365dcba3b48768507d54ee1516529d817c2
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-05-06 10:06:27 +0530

    chat-server

--- Backend/chat_backend.js
+++ Backend/chat_backend.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const fs=require("fs")
+const path=require("path")
 const  socketio= require("socket.io");
 const http = require("http");
 const app=express()
+ app.use(express.json())
+const Path=path.join(__dirname,"../Frontend")
+ 
+app.use(express.static(Path+"/public"))
+
+console.log(Path,"dflsjkd")
 
 
 
 const server = http.createServer(app)
+
+
+app.get("/chat/frontend", async(req,res)=>{
+
+      try {
+        // res.send("hello")
+        const frontendPath = path.join(__dirname, '../Frontend/chat.html');
+        console.log(frontendPath)
+        app.use((req, res, next) => {
+            if (req.url.endsWith('.css')) {
+              res.type('text/css');
+            }
+            next();
+          });
+        res.sendFile(frontendPath)
+      } catch (error) {
+        console.log(error)
+      }
+})
 
 const io =   socketio(server);
 
@@ -13,7 +40,9 @@ const io =   socketio(server);
 
 
 
-app.listen(4500,()=>{
+
+
+server.listen(4500,()=>{
      
-    console.log("chat server is running")
+    console.log("chat server is running 4500")
 })
