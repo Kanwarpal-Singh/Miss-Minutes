70ae79fd33df040ab6ed0a80f54277ef08d9ff45
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2023-05-08 00:17:17 +0530

    day4

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -1,28 +1,12 @@
-const myDiv = document.getElementsByClassName("navbar_right")[0];
-let Navbar_divVisible = false;
-if (window.innerWidth > 1184) {
-  myDiv.style.display = "flex";
-}
+let wrapper_status;
 
-// Show/hide div on toggle
-function toggleDiv() {
-  if (Navbar_divVisible) {
-    myDiv.style.display = "none";
-    Navbar_divVisible = false;
-  } else {
-    myDiv.style.display = "block";
-    Navbar_divVisible = true;
+function Wrapper_navbar() {
+  const navbar_wrapper = document.getElementsByClassName("navbar_right")[0];
+  if (wrapper_status) {
+    navbar_wrapper.style.display = "none";
+    wrapper_status = false;
+    return;
   }
+  navbar_wrapper.style.display = "flex";
+  wrapper_status = true;
 }
-
-// Show div if screen size is changed back to normal
-function handleResize() {
-  if (window.innerWidth > 1184) {
-    myDiv.style.display = "flex";
-  } else {
-    myDiv.style.display = Navbar_divVisible ? "block" : "none";
-  }
-}
-
-// Add event listener for resize event
-window.addEventListener("resize", handleResize);
