beb9c707db31fbcf56041ae03a2ac3602297397d
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-05-07 10:29:43 +0530

    chat-api

--- Backend/chat_backend.js
+++ Backend/chat_backend.js
@@ -21,7 +21,7 @@ app.get("/chat/frontend", async(req,res)=>{
 
       try {
         const frontendPath = path.join(__dirname, '../Frontend/chat.html');
-        console.log(frontendPath)
+      //  console.log(frontendPath)
   
         res.sendFile(frontendPath)
       } catch (error) {
@@ -35,15 +35,52 @@ app.get("/chat/frontend", async(req,res)=>{
 const io =   socketio(server);
 
 
- io.on("connection",(socket)=>{
-
-       console.log(socket.id,"socket id")
-
- })
 
 
 
+ let users = [];
 
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //Taking  userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //Sending  and getting message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  // disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
 
 
 server.listen(4500,()=>{
--- Frontend/public/scripts/chat.js
+++ Frontend/public/scripts/chat.js
@@ -1,7 +1,12 @@
 
 // *  chat appliction js file     
-
+// const axios=require("axios")
 
 
 const  socket= io("ws://localhost:4500",{transports:["websocket"]})
 
+
+       socket.on("Welcome",(message)=>{
+
+              console.log(message,"hlloo")
+       }) 
